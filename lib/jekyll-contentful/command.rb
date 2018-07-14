31210a401d17fce1e54c2341f6928a5713ebd185
Taylor MacDonald <tcmacdonald@gmail.com>
2018-07-13 21:37:08 -0400

    US14173 Associations Add cf alias

--- lib/jekyll-contentful/command.rb
+++ lib/jekyll-contentful/command.rb
@@ -7,6 +7,7 @@ module Jekyll
 
         def init_with_program(prog)
           prog.command(:contentful) do |c|
+            c.alias(:cf)
             c.syntax "contentful [options]"
             c.description 'Imports data from Contentful'
             c.option 'collections', '-c', '--collections COL1[,COL2]', Array, 'Only return content for specified collections'
