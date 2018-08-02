0a04ac8ec00e92e5ebcdd2c332c9caec44e52af5
Taylor C. MacDonald <tcmacdonald@gmail.com>
2018-08-02 14:52:46 -0400

    US14173 Associations Refactor content_types, add specs

--- lib/jekyll-contentful/content_types.rb
+++ lib/jekyll-contentful/content_types.rb
@@ -6,80 +6,93 @@ module Jekyll
     class ContentTypes
       class << self
 
-        attr_accessor :space, :entries, :config
-
-        def space
-          @space ||= begin
-            management.spaces.find(ENV['CONTENTFUL_SPACE_ID'])
-          end
-        end
-
-        def management
-          @management ||= begin
-            ::Contentful::Management::Client.new(ENV['CONTENTFUL_MANAGEMENT_TOKEN'])
-          end
-        end
-
-        def load_jekyll_config(dir=nil)
-          @config ||= begin
-            file = File.join(dir || File.expand_path(__dir__), '_config.yml')
-            yml = File.exist?(file) ? YAML.load(File.read(file)) : {}
-            yml.dig('contentful') || {}
-          end
-        end
+        attr_accessor :space, :entries, :config, :models
 
         def all(project_dir=nil)
           load_jekyll_config(project_dir)
 
           @entries ||= begin
-            models = space.content_types.all
-            schema = models.collect do |model|
-              fields = []
-              references = []
-              model.properties.dig(:fields).each do |field|
-                if %w(Array Link).include?(field.type) && field.properties.dig(:linkType) != 'Asset'
-                  references.push(field)
-                else
-                  fields.push(field)
-                end
-              end
-
-              [model.id, {
-                "fields" => fields.collect(&:id),
-                "references" => references.collect{|ref|
-                  if ref.type == 'Array'
-                    link_content_types = ref.items.validations.collect{|v| v.properties.dig(:linkContentType) }.flatten
-                  else
-                    link_content_types = ref.validations.collect{|v| v.properties.dig(:linkContentType) }.flatten
-                  end
-
-                  if link_content_types.empty?
-                    ref.id
-                  else
-                    Hash[ref.id, link_content_types]
-                  end
-                }
-              }]
-            end
-
-            schema.reject!{|arr|
-              (config.dig('exclude') || []).include? arr.first
-            }
-            schema_obj = Hash[schema]
-
+            schema = Hash[get_schema]
             Hash[schema.collect{|name,obj|
               obj['references'] = obj['references'].collect{|type|
                 if type.is_a? Hash
                   type, models = type.first
-                  Hash[type, models.collect{|model| Hash[model, schema_obj[model]['fields']] }]
+                  Hash[type, models.collect{|model| Hash[model, schema[model]['fields']] }]
                 else
-                  Hash[type, schema_obj[type.singularize]['fields']]
+                  Hash[type, schema[type.singularize]['fields']]
                 end
               }.reduce({}, :merge)
               [name, obj]
             }]
           end
         end
+
+        private
+
+          def get_models
+            space.content_types.all
+          end
+
+          def get_fields(model)
+            output = OpenStruct.new(fields: [], references: [])
+            model.properties.dig(:fields).each do |field|
+              if %w(Array Link).include?(field.type) && field.properties.dig(:linkType) != 'Asset'
+                output.references.push(field)
+              else
+                output.fields.push(field)
+              end
+            end
+            output
+          end
+
+          def get_schema
+            @models = get_models
+            schema = @models.collect do |model|
+              model_details = get_fields(model)
+              [model.id, {
+                "fields" => model_details.fields.collect(&:id),
+                "references" => model_details.references.collect(&parse_reference_field)
+              }]
+            end
+            schema.reject{|arr| (config.dig('exclude') || []).include? arr.first }
+          end
+
+          def parse_reference_field
+            -> (field) {
+              content_types = begin
+                if field.type == 'Array'
+                  field.items.validations.collect{|v| v.properties.dig(:linkContentType) }.flatten
+                else
+                  field.validations.collect{|v| v.properties.dig(:linkContentType) }.flatten
+                end
+              end
+              if content_types.empty?
+                field.id
+              else
+                Hash[field.id, content_types]
+              end
+            }
+          end
+
+          def space
+            @space ||= begin
+              management.spaces.find(ENV['CONTENTFUL_SPACE_ID'])
+            end
+          end
+
+          def management
+            @management ||= begin
+              ::Contentful::Management::Client.new(ENV['CONTENTFUL_MANAGEMENT_TOKEN'])
+            end
+          end
+
+          def load_jekyll_config(dir=nil)
+            @config ||= begin
+              file = File.join(dir || File.expand_path(__dir__), '_config.yml')
+              yml = File.exist?(file) ? YAML.load(File.read(file)) : {}
+              yml.dig('contentful') || {}
+            end
+          end
 
       end
     end
---
+++ spec/unit/content_types_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+require 'jekyll'
+require 'active_support/inflector'
+
+describe Jekyll::Contentful::ContentTypes do
+
+  before do
+    @klass = Jekyll::Contentful::ContentTypes
+  end
+
+  it 'should return the CF space instance' do
+    VCR.use_cassette 'contentful/space' do
+      expect(@klass.send(:space)).to be_a(Contentful::Management::Space)
+    end
+  end
+
+  it 'should return the management client' do
+    expect(@klass.send(:management)).to be_a(Contentful::Management::Client)
+  end
+
+  it 'should load Jekyll config file' do
+    path = File.expand_path(__dir__), '../dummy'
+    cfg = @klass.send(:load_jekyll_config, path)
+    expect(cfg).to be_a(Object)
+    expect(cfg.keys).to include('exclude')
+  end
+
+  it 'should return all entries from Contentful' do
+    path = File.expand_path(__dir__), '../dummy'
+    VCR.use_cassette 'contentful/types' do
+      content_types = @klass.all(path)
+      %w(testable product article author widget).each do |type|
+        expect(content_types.keys).to include(type)
+      end
+      article = content_types['article']
+      expect(article.dig('fields')).to match_array(['title'])
+      expect(article.dig('references', 'author')).to match_array([{"author"=>["full_name"]}])
+      expect(article.dig('references', 'widgets')).to match_array([{"widget"=>["title"]}, {"testable"=>["title"]} ])
+    end
+  end
+
+  it 'should return fields & references for a content_type' do
+    model = nil
+    VCR.use_cassette 'contentful/types' do
+      model = @klass.send(:get_models).detect{|m| m.id == 'article' }
+    end
+    refs = @klass.send(:get_fields, model)
+    expect(refs).to be_a(OpenStruct)
+    expect(refs.fields).to be_a(Array)
+    expect(refs.fields.all?{|f| f.is_a?(Contentful::Management::Field) }).to be true
+    expect(refs.references).to be_a(Array)
+    expect(refs.references.all?{|f| f.is_a?(Contentful::Management::Field) }).to be true
+  end
+
+  it 'should get all models from Contentful' do
+    VCR.use_cassette 'contentful/types' do
+      models = @klass.send(:get_models)
+      expect(models).to be_a(Contentful::Management::Array)
+    end
+  end
+
+  it 'should return schema, sans excluded content-types' do
+    VCR.use_cassette 'contentful/types' do
+      allow(@klass).to receive(:config).and_return({ 'exclude' => ['testable', 'widget'] })
+      schema = @klass.send(:get_schema)
+      keys = schema.collect(&:first)
+      expect(keys).to match_array(['product','article','author'])
+      expect(keys).to_not include('testable')
+      expect(keys).to_not include('widget')
+    end
+  end
+
+  it 'should parse and return field references via block' do
+    VCR.use_cassette 'contentful/types' do
+      models = @klass.send(:get_models)
+      model = models.detect{|m| m.id == 'article' }
+      fields = @klass.send(:get_fields, model)
+      references = fields.references.collect(&@klass.send(:parse_reference_field))
+      expect(references).to match_array([{"author"=>["author"]}, {"widgets"=>["testable", "widget"]}])
+    end
+  end
+
+end
