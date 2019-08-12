c0685b1aac9fa9e356522fe77e2e9ebe2fc7f182
Warren Harrison <warren@hungry-media.com>
2019-08-12 15:11:55 -0400

    Allow for empty excluded_models

--- lib/jekyll-contentful/content_types.rb
+++ lib/jekyll-contentful/content_types.rb
@@ -65,7 +65,7 @@ module Jekyll
           end
 
           def excluded_models(schema)
-            if config.dig('exclude').include?('*')
+            if config.dig('exclude').try('include?','*')
               models = schema.collect(&:first)
               specified_models = @config.except('exclude').keys
               models.difference(specified_models)
